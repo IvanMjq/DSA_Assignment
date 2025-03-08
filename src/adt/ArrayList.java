a62ea3a7d530ea181bc695d10aa660dbd1de592a
IvanMjq <149957584+IvanMjq@users.noreply.github.com>
2025-03-08 15:36:07 +0800

    Add files via upload

---
+++ src/adt/ArrayList.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adt;
+
+/**
+ *
+ * @author ivanmjq
+ */
+
+import java.io.Serializable;
+
+public class ArrayList<T> implements ListInterface<T>, Serializable {
+    
+}
---
+++ src/entity/Company.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author ivanmjq
+ */
+public class Company implements Serializable {
+    
+    private String id;
+    private String companyName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Company)) {
+            return false;
+        }
+        Company other = (Company) object;
+        return !((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id)));
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Company[ id=" + id + " ]";
+    }
+    
+}
