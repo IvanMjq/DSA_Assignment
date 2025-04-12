17de2c22ea950e6da70a793f6b7a8afadf7ab75a
zq2208 <limziqi0804@gmail.com>
2025-04-12 15:08:28 +0800

    Create InterviewSchedulingControl.java

---
+++ src/control/InterviewSchedulingControl.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package control;
+
+import adt.DoublyLinkedList;
+import adt.ListInterface;
+import entity.Interview;
+import java.util.Scanner;
+import java.util.Calendar;
+
+/**
+ *
+ * @author Lim Zi Qi
+ */
+public class InterviewSchedulingControl {
+    
+    private ListInterface<Interview> Interview = new DoublyLinkedList<>();
+    static int count = 1;
+
+    public void interviewScheduling(MatchingResult matchingResult){
+    
+        System.out.println("============================");
+        System.out.println("   Interview Scheduling");
+        System.out.println("============================/n");
+        
+        for(int i = 0; i <= matchingResult.size(); i++){
+            
+        }
+        
+    }
+        
+    public void interviewTimesReprot(){
+        
+        
+        
+    }
+    
+    
+}
