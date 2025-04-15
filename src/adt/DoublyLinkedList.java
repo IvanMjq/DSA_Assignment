d0822ceb18727096ce91ccba2b6566b901be8814
IvanMjq <2004ivanmok@gmail.com>
2025-04-15 23:27:26 +0800

    update entity

--- src/adt/DoublyLinkedList.java
+++ src/adt/DoublyLinkedList.java
@@ -173,21 +173,7 @@ public class DoublyLinkedList<T extends Comparable<T>> implements ListInterface<
         return -1; // Return -1 if the data is not found in the list
     }
     
-//    @Override
-//    public ListInterface<T> insertionSort() {
-//        DoublyLinkedList<T> sortedList = new DoublyLinkedList<>();
-//        
-//        for (T currentData : this) {
-//            Node newNode = new Node(currentData);
-//            
-//            // Case 1 : empty list
-//            if (sortedList.isEmpty()) {
-//                sortedList.head = sortedList.tail = newNode;
-//            }
-//        }
-//        
-//        return sortedList;
-//    }
+    
 
     @Override
     public Iterator<T> iterator() {
